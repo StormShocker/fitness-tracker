74e41bc41ac6c496456dcbc543a82162a07a6edf
Sean Gonzalez <sean.oryn@gmail.com>
2020-07-09 08:25:58 -0500

    Final product

---
+++ routes/api-route.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+
+const Workout = require("../models/workout.js");
+
+
+router.get("/api/workouts", (req, res) => {
+	Workout.find()
+		.then((allWorkouts) => {
+			res.json(allWorkouts);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+
+router.post("/api/workouts", ({ body }, res) => {
+	Workout.create(body)
+		.then((newWorkout) => {
+			console.log(newWorkout);
+			res.json(newWorkout);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+
+router.put("/api/workouts/:id", (req, res) => {
+	Workout.updateOne(
+		{ _id: req.params.id },
+		{ $push: { exercises: req.body } },
+		{ new: true }
+	)
+		.then((newExercise) => {
+			console.log(newExercise);
+			res.json(newExercise);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+
+router.get("/api/workouts/range", (req, res) => {
+	Workout.find()
+		.then((allWorkouts) => {
+			res.json(allWorkouts);
+		})
+		.catch((err) => {
+			res.status(400).json(err);
+		});
+});
+
+module.exports = router;
